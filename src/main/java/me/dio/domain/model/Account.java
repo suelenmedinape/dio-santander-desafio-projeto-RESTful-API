bc9fdc0682be5c214e811d19a4f0dd96594ad86a
Suelen Medina <suelenmedinape@gmail.com>
2024-07-11 10:41:23 -0300

    update

--- src/main/java/me/dio/domain/model/Account.java
+++ src/main/java/me/dio/domain/model/Account.java
@@ -1,13 +1,25 @@
 package me.dio.domain.model;
 
 
+import jakarta.persistence.*;
+
 import java.math.BigDecimal;
 
+@Entity(name = "tb_account")
 public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(unique = true)
     private String number;
     private String agency;
+
+    @Column(precision = 2, scale = 13)
     private BigDecimal balance;
+
+    @Column(name = "additional_limit", precision = 2, scale = 13)
     private BigDecimal limit;
 
     public String getNumber() {
